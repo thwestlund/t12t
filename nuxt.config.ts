e6aa254e8e22e5dc34a0335a8aa5be42b016b0cf
ThommyWestlund <thommy.westlund@b3.se>
2025-03-12 09:31:33 +0100

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxt/content"],
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+});
