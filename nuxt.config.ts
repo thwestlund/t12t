c0cef847a85d476431f234bc64b46d3a8536430f
ThommyWestlund <thommy.westlund@b3.se>
2025-03-19 13:40:23 +0100

    configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,4 +45,22 @@ export default defineNuxtConfig({
   },
 
   compatibilityDate: "2025-03-12",
+
+  content: {
+    build: {
+      markdown: {
+        // Object syntax can be used to override default options
+        remarkPlugins: {
+          // Override remark-emoji options
+          "remark-emoji": {
+            options: {
+              emoticon: true,
+            },
+          },
+          // Disable remark-gfm
+          "remark-gfm": false,
+        },
+      },
+    },
+  },
 });
