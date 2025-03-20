e17bf87898c37f5fc1aab4ffdf5e777306452b0a
ThommyWestlund <thommy.westlund@b3.se>
2025-03-20 09:06:35 +0100

    removed content

--- pages/index.vue
+++ pages/index.vue
@@ -7,21 +7,21 @@
 
     <div class="container-main py-16">
       <!-- Main sections -->
-      <h2 class="text-3xl font-bold text-center mb-12">Utforska våra huvudområden</h2>
+      <!--h2 class="text-3xl font-bold text-center mb-12">Utforska våra huvudområden</h2>
 
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-12">
         <!-- Knowledge Bank Section -->
-        <Card title="Kunskapsbank"
+      <!--Card title="Kunskapsbank"
           description="Omfattande information om tillgänglighetsstandarder, lagstiftning och bästa praxis för webbutvecklare, designers och innehållsskapare."
           linkTo="/kunskapsbank" linkText="Utforska kunskapsbanken" icon="material-symbols:menu-book-outline" />
 
         <!-- Screen Readers Section -->
-        <!--Card title="Skärmläsare"
+      <!--Card title="Skärmläsare"
           description="Guider för hur olika skärmläsare fungerar, kortkommandon och testmetoder för att säkerställa att dina webbplatser fungerar för alla användare."
           linkTo="/skärmläsare" linkText="Lär dig om skärmläsare" icon="material-symbols:accessibility-new-rounded" /-->
 
-        <!-- Testing Section -->
-        <Card title="Testning"
+      <!-- Testing Section -->
+      <!--Card title="Testning"
           description="Metoder och verktyg för att testa och utvärdera tillgängligheten på dina webbplatser och applikationer, både automatiskt och manuellt."
           linkTo="/testning" linkText="Utforska testmetoder" icon="material-symbols:code" />
       </div>
